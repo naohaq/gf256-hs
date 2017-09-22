be561a0168abdc87a57cecf94933b04df2e7eb16
naohaq <naohaq@ps.sakura.ne.jp>
2017-09-22 16:22:12 +0900

    Reduce template parameter.

--- src/Data/GF256/GP256Insts.hs
+++ src/Data/GF256/GP256Insts.hs
@@ -6,42 +6,23 @@ module Data.GF256.GP256Insts
   ,PP369, PP391, PP397, PP425, PP451, PP463, PP487, PP501
   ) where
 
-import TypeLevel.Number.Nat
-
 import Data.GF256.GenPoly256
 
-newtype PP285 = PP285 (I (O (I (I (I (O (O (O (I Z)))))))))
-newtype PP299 = PP299 (I (I (O (I (O (I (O (O (I Z)))))))))
-newtype PP301 = PP301 (I (O (I (I (O (I (O (O (I Z)))))))))
-newtype PP333 = PP333 (I (O (I (I (O (O (I (O (I Z)))))))))
-newtype PP351 = PP351 (I (I (I (I (I (O (I (O (I Z)))))))))
-newtype PP355 = PP355 (I (I (O (O (O (I (I (O (I Z)))))))))
-newtype PP357 = PP357 (I (O (I (O (O (I (I (O (I Z)))))))))
-newtype PP361 = PP361 (I (O (O (I (O (I (I (O (I Z)))))))))
-newtype PP369 = PP369 (I (O (O (O (I (I (I (O (I Z)))))))))
-newtype PP391 = PP391 (I (I (I (O (O (O (O (I (I Z)))))))))
-newtype PP397 = PP397 (I (O (I (I (O (O (O (I (I Z)))))))))
-newtype PP425 = PP425 (I (O (O (I (O (I (O (I (I Z)))))))))
-newtype PP451 = PP451 (I (I (O (O (O (O (I (I (I Z)))))))))
-newtype PP463 = PP463 (I (I (I (I (O (O (I (I (I Z)))))))))
-newtype PP487 = PP487 (I (I (I (O (O (I (I (I (I Z)))))))))
-newtype PP501 = PP501 (I (O (I (O (I (I (I (I (I Z)))))))))
-
-declInstGP256 ''PP285 'PP285
-declInstGP256 ''PP299 'PP299
-declInstGP256 ''PP301 'PP301
-declInstGP256 ''PP333 'PP333
-declInstGP256 ''PP351 'PP351
-declInstGP256 ''PP355 'PP355
-declInstGP256 ''PP357 'PP357
-declInstGP256 ''PP361 'PP361
-declInstGP256 ''PP369 'PP369
-declInstGP256 ''PP391 'PP391
-declInstGP256 ''PP397 'PP397
-declInstGP256 ''PP425 'PP425
-declInstGP256 ''PP451 'PP451
-declInstGP256 ''PP463 'PP463
-declInstGP256 ''PP487 'PP487
-declInstGP256 ''PP501 'PP501
+declInstGP256 285
+declInstGP256 299
+declInstGP256 301
+declInstGP256 333
+declInstGP256 351
+declInstGP256 355
+declInstGP256 357
+declInstGP256 361
+declInstGP256 369
+declInstGP256 391
+declInstGP256 397
+declInstGP256 425
+declInstGP256 451
+declInstGP256 463
+declInstGP256 487
+declInstGP256 501
 
 -- EOF
--- src/Data/GF256/GenPoly256.hs
+++ src/Data/GF256/GenPoly256.hs
@@ -29,14 +29,34 @@ class GenPoly256 k where
   logTable :: k -> UA.UArray Int Int
   powTable :: k -> UA.UArray Int Int
 
-declInstGP256 :: TH.Name -> TH.Name -> TH.DecsQ
-declInstGP256 nameT nameV =
+patUnbox :: TH.Name -> String -> TH.PatQ
+patUnbox cname vname = TH.conP cname [TH.varP =<< TH.newName vname]
+
+noBang :: TH.Q TH.Bang
+noBang = TH.bang TH.noSourceUnpackedness TH.noSourceStrictness
+
+declInstGP256' :: TH.Name -> TH.Name -> TH.DecsQ
+declInstGP256' nameT nameV =
   [d|
     instance GenPoly256 $(TH.conT nameT) where
       genVal = $(TH.conE nameV) undefined
-      genInt ($(TH.conP nameV [TH.newName "x" >>= TH.varP])) = toInt x
+      genInt $(patUnbox nameV "x") = toInt x
       logTable x = genLogTable $ genInt x
       powTable x = genPowTable $ genInt x
    |]
+
+declNT :: TH.Name -> TH.Name -> Integer -> TH.DecQ
+declNT tcon vcon n = TH.newtypeD cxt tcon [] Nothing (body vcon) (sequence [])
+  where cxt = TH.cxt []
+        tp  = TH.bangType noBang (natT n)
+        body nm = TH.normalC nm [tp]
+
+declInstGP256 :: Integer -> TH.DecsQ
+declInstGP256 n = do
+  tcon <- TH.newName $ "PP" ++ show n
+  vcon <- TH.newName $ "PP" ++ show n
+  ntdec <- declNT tcon vcon n
+  instds <- declInstGP256' tcon vcon
+  return $ ntdec : instds
 
 -- EOF
