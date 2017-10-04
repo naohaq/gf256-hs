0d7d19449cf8cc47d926ef74a59d236328959b1d
naohaq <naohaq@ps.sakura.ne.jp>
2017-10-04 13:35:15 +0900

    Organized functions into the type class 'GF2Polynomial'.

--- src/Data/GF2Polynomial.hs
+++ src/Data/GF2Polynomial.hs
@@ -31,62 +31,110 @@
 -----------------------------------------------------------------------------
 
 module Data.GF2Polynomial
-  ( mul
-  , add
-  , divMod
-  , mod
+  ( GF2Polynomial (..)
   , order
   ) where
 
 import Prelude hiding (mod, divMod)
 import Data.Bits
+import Data.Word
 
-highBit :: (Bits a, Num a, Integral b) => a -> b
-highBit 0 = 0
-highBit 1 = 0
-highBit n = 1 + highBit (n `shiftR` 1)
+class GF2Polynomial k where
+  -- |Returns the leftmost position of the bit '1'
+  highestBit :: k -> Int
+  -- |Multiplies two numbers as polynomials over GF(2).
+  mul :: k -> k -> k
+  -- |Adds two numbers as polynomials over GF(2).
+  --
+  -- @(a \`add\` b)@ is equivalent to @(a \`xor\` b)@.
+  add :: k -> k -> k
+  -- |Subtract a number from another number as polynomials over GF(2).
+  --
+  -- @(a \`sub\` b)@ is equivalent to @(a \`add\` b)@.
+  sub :: k -> k -> k
+  sub x y = add x y
+  -- |Calculates the quotient and the remainder of A/B.
+  --
+  -- For given \(A(x)\), \(B(x)\neq{0}\), there exists
+  -- the quotient \(Q(x)\) and the remainder \(R(x)\)
+  -- such that: \(A(x) = Q(x)\cdot{B}(x) + R(x)\).
+  divMod :: k -> k -> (k, k)
+  -- |The function returns only quotient part of division.
+  div :: k -> k -> k
+  div x y = fst $ divMod x y
+  -- |The function returns only remainder part of division.
+  mod :: k -> k -> k
+  mod x y = snd $ divMod x y
 
--- |Multiplies two numbers as polynomials over GF(2).
-infixl 7 `mul`
-mul :: (Bits a, Num a) => a -> a -> a
-mul x y = iter 0 x y
+instance GF2Polynomial Integer where
+  highestBit 0 = 0
+  highestBit 1 = 0
+  highestBit n = 1 + highestBit (n `shiftR` 1)
+  mul    x y = mul_f2 x y
+  add    x y = add_f2 x y
+  divMod x y = divMod_f2 x y
+
+instance GF2Polynomial Int where
+  highestBit x = finiteBitSize x - 1 - countLeadingZeros x
+  mul    x y = mul_f2 x y
+  add    x y = add_f2 x y
+  divMod x y = divMod_f2 x y
+
+instance GF2Polynomial Word where
+  highestBit x = finiteBitSize x - 1 - countLeadingZeros x
+  mul    x y = mul_f2 x y
+  add    x y = add_f2 x y
+  divMod x y = divMod_f2 x y
+
+instance GF2Polynomial Word64 where
+  highestBit x = finiteBitSize x - 1 - countLeadingZeros x
+  mul    x y = mul_f2 x y
+  add    x y = add_f2 x y
+  divMod x y = divMod_f2 x y
+
+instance GF2Polynomial Word32 where
+  highestBit x = finiteBitSize x - 1 - countLeadingZeros x
+  mul    x y = mul_f2 x y
+  add    x y = add_f2 x y
+  divMod x y = divMod_f2 x y
+
+instance GF2Polynomial Word16 where
+  highestBit x = finiteBitSize x - 1 - countLeadingZeros x
+  mul    x y = mul_f2 x y
+  add    x y = add_f2 x y
+  divMod x y = divMod_f2 x y
+
+instance GF2Polynomial Word8 where
+  highestBit x = finiteBitSize x - 1 - countLeadingZeros x
+  mul    x y = mul_f2 x y
+  add    x y = add_f2 x y
+  divMod x y = divMod_f2 x y
+
+mul_f2 :: (Bits a, Num a) => a -> a -> a
+mul_f2 x y = iter 0 x y
   where iter !prod  _  0 = prod
         iter !prod !a !b
-          | (b .&. 1) /= 0  = iter (prod `add` a) a' b'
-          | otherwise       = iter  prod          a' b'
+          | (b .&. 1) /= 0  = iter (prod `add_f2` a) a' b'
+          | otherwise       = iter  prod             a' b'
           where a' = a `shiftL` 1
                 b' = b `shiftR` 1
 
--- |Adds two numbers as polynomials over GF(2).
---
--- @(a \`add\` b)@ is equivalent to @(a \`xor\` b)@.
-infixl 6 `add`
-add :: (Bits a, Num a) => a -> a -> a
-add x y = x `xor` y
+add_f2 :: (Bits a, Num a) => a -> a -> a
+add_f2 x y = x `xor` y
 
--- |Calculates the quotient and the remainder of A/B.
---
--- For given \(A(x)\), \(B(x)\neq{0}\), there exists
--- the quotient \(Q(x)\) and the remainder \(R(x)\)
--- such that: \(A(x) = Q(x)\cdot{B}(x) + R(x)\).
-divMod :: (Integral a, Bits a) => a -> a -> (a, a)
-divMod _ 0 = undefined
-divMod x y | x < 0     = divMod (-x) y
-           | y < 0     = divMod x (-y)
-           | otherwise = iter n (n-m) 0 x
-  where n = highBit x
-        m = highBit y
+divMod_f2 :: (GF2Polynomial a, Bits a, Integral a) => a -> a -> (a, a)
+divMod_f2 _ 0 = error "divide by 0"
+divMod_f2 x y | x < 0     = divMod_f2 (-x) y
+              | y < 0     = divMod_f2 x (-y)
+              | otherwise = iter n (n-m) 0 x
+  where n = fromIntegral $ highestBit x
+        m = fromIntegral $ highestBit y
         iter !t !s !q !r | s < 0     = (q,r)
                          | b /= 0    = iter (t-1) (s-1) q' r'
                          | otherwise = iter (t-1) (s-1) q r
           where b  = r .&.   (1 `shiftL` t)
                 q' = q .|.   (1 `shiftL` s)
                 r' = r `xor` (y `shiftL` s)
-
--- |The function returns only remainder part of division.
-infixl 7 `mod`
-mod :: (Integral a, Bits a) => a -> a -> a
-mod x y = snd $ divMod x y
 
 -- |Calculate <http://mathworld.wolfram.com/MultiplicativeOrder.html multiplicative order>
 -- of \(x\) (mod \(P(x)\)). 0 will be returned for an even number.
